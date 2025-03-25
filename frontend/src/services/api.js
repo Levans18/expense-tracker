2686f3a60d67edef515db83cbcaf94a653907fbf
Liam Evans <51797340+Levans18@users.noreply.github.com>
2025-03-25 13:12:33 -0400

    Fixed a routing error for prod

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,4 +3,4 @@ const isLocalhost =
 
     export const API_BASE_URL = isLocalhost
     ? "http://localhost:8080"
-    : "/api"; // This gets rewritten by vercel.json in prod
+    : "/prod"; // This gets rewritten by vercel.json in prod
