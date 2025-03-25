83eaae036cf9b0d9fd321f8c758495d940321d08
Liam Evans <51797340+Levans18@users.noreply.github.com>
2025-03-25 15:31:55 -0400

    Enable env.properties to be used

--- backend/src/main/java/com/expense/tracker/ExpenseTrackerApplication.java
+++ backend/src/main/java/com/expense/tracker/ExpenseTrackerApplication.java
@@ -2,8 +2,10 @@ package com.expense.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:env.properties") // 👈 Add this
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
